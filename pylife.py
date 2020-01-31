bf85a757ccfb0288a302ccd0e47ffdb4e1537a01
Uwila <uwila@runbox.com>
2020-01-31 17:32:40 +0100

    Add hud and controls for refresh rate

--- pylife.py
+++ pylife.py
@@ -5,21 +5,20 @@ import time
 
 interval = 0.1
 
-def main(screen):
-    global interval, old_time
+show_hud = False
 
+def main(screen):
+    global interval, show_hud
+
+    screen.nodelay(1)
     curses.curs_set(0)
     screen.clear()
 
     height, width = screen.getmaxyx()
-    #height, width = height - 1, width - 1
 
     grid = [[randint(0,1) for _ in range(height)] for _ in range (width)]
 
-    # Draw grid
-    for x in range(width):
-        for y in range(height):
-            draw(screen,x,y,'X' if grid[x][y] else ' ')
+    draw_screen(screen, grid)
 
     while True:
         # Resize if needed
@@ -56,22 +55,48 @@ def main(screen):
         for x,y,value in updates:
             if value == 0:
                 grid[x][y] = 0
-                draw(screen,x,y,' ')
             else:
                 grid[x][y] = 1
-                draw(screen,x,y,'X')
+
+        draw_screen(screen,grid)
 
         # Update screen and wait to get desired refresh rate
         screen.refresh()
-        new_time = time.time()
-        time.sleep(max(0, interval - new_time + old_time))
-    screen.getkey()
+
+        while (key := screen.getch()) != curses.ERR or interval - (new_time:=time.time()) + old_time > 0:
+            if key == curses.ERR:
+                continue
+            elif key == ord('h'):
+                show_hud = not show_hud
+            elif key == ord('.'):
+                interval *= 1.1
+            elif key == ord(','):
+                interval /= 1.1
+            draw_screen(screen, grid)
+
 
 def draw(screen,x,y,c):
     try:
         screen.addstr(y, x, c)
     except:
         pass
+
+def draw_screen(screen, grid):
+    global interval
+
+    width = len(grid)
+    height = len(grid[0])
+
+    # Draw grid
+    for x in range(width):
+        for y in range(height):
+            draw(screen,x,y,'X' if grid[x][y] else ' ')
+
+    # Draw settings
+    if show_hud:
+        draw(screen,1,1,'Target refresh interval: {}'.format(interval))
+        draw(screen,1,2,'Current resolution: {}x{}'.format(width,height))
+
 
 
 curses.wrapper(main)
